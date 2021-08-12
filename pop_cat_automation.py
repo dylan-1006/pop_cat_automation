74f8663fb2a6a110647cdac7c68ce6a77229a07d
Dylan <88710142+dylan-1006@users.noreply.github.com>
2021-08-12 15:15:30 +0800

    Update pop_cat_automation.py

--- pop_cat_automation.py
+++ pop_cat_automation.py
@@ -1,14 +1,14 @@
 import pyautogui
-x=0
-screen_width, screen_height= pyautogui.size()
-print(screen_width , screen_height)
-pyautogui.moveTo()
-current_X_position, current_Y_position =pyautogui.position()
+#run the below two lines and get the coordinates of the postion to click 
+current_X_position, current_Y_position =pyautogui.position() 
 print(current_X_position,current_Y_position)
-while x!=1000:
-    pyautogui.click(1708,234)
+
+count=0
+while count!=1000: #set value for amount of times to click
+    pyautogui.click(1708,234) #insert (X,Y) coordinates obtained from above
     current_X_position, current_Y_position = pyautogui.position()
     x+=1
-    if current_X_position!=  1708 or current_Y_position!= 234:
+    if current_X_position!=  1708 or current_Y_position!= 234: 
+        #insert (X,Y) coordinates obtained from above, this will stop the loop when u move ur mouse
         break
 
